e668469e56ba9986c4200f90c22da888e60c75ca
Luciano Claudio <lucianoclaudio.se@gmail.com>
2025-10-01 10:31:56 -0300

    Implementando HexGrid

--- Assets/Scripts/UI/ClockUI.cs
+++ Assets/Scripts/UI/ClockUI.cs
@@ -5,14 +5,27 @@ public class ClockUI : MonoBehaviour
 {
     public TimeManager timeManager;
     public TMP_Text clockText;
+    public TMP_Text dayText;
 
     void OnEnable() => GameEvents.OnClockChanged += UpdateClock;
     void OnDisable() => GameEvents.OnClockChanged -= UpdateClock;
 
+    private int day, hour, minute;
+
     void Start() => UpdateClock(timeManager.DayCount, timeManager.Hour, timeManager.Minute);
 
-    void UpdateClock(int day, int hour, int minute)
+    private void Update()
     {
-        clockText.text = $"Dia {day}  {hour:00}:{minute:00}";
+
+        clockText.text = $" {hour:00}:{minute:00}";
+        dayText.text = $"Day {day}";
+
+    }
+
+    public void UpdateClock(int day, int hour, int minute)
+    {
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
     }
 }
